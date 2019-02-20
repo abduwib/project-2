fa6aff9afbc9d92543e14b7faa348f260eb6d04f
abduwibr <abduwibr@gmail.com>
2019-02-20 16:41:57 +0300

    Updates

---
+++ precache-manifest.93ab061cad3cb933d5fef050f1bf45cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "342290bb1bd741152a63",
+    "url": "/project-2/static/css/main.3199cec5.chunk.css"
+  },
+  {
+    "revision": "342290bb1bd741152a63",
+    "url": "/project-2/static/js/main.342290bb.chunk.js"
+  },
+  {
+    "revision": "8204681cae27f55d9384",
+    "url": "/project-2/static/js/runtime~main.8204681c.js"
+  },
+  {
+    "revision": "65f36c3a20294139ab77",
+    "url": "/project-2/static/css/2.5ce81abe.chunk.css"
+  },
+  {
+    "revision": "65f36c3a20294139ab77",
+    "url": "/project-2/static/js/2.65f36c3a.chunk.js"
+  },
+  {
+    "revision": "f57a050f9a1770842bc550a2d2c21806",
+    "url": "/project-2/static/media/icon3.f57a050f.png"
+  },
+  {
+    "revision": "0f7acbf327e6347eee0128d6e54c066f",
+    "url": "/project-2/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/project-2/precache-manifest.93ab061cad3cb933d5fef050f1bf45cf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/project-2/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
